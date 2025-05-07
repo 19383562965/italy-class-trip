80842a76cfb58eb17662a8cc880d14a32c7a7a27
Timurs Koleda <timurs.koleda@gmail.com>
2025-05-07 20:51:45 +0200

    Delete itay project directory

--- itay project/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('open-all-img').onclick = function() {
-  document.getElementById('open-all-img').style.display = 'none'
-  document.getElementById('all-img-parent').style.display = 'flex'
-}
-document.getElementById('close-all-img').onclick = function() {
-  document.getElementById('open-all-img').style.display = 'flex'
-  document.getElementById('all-img-parent').style.display = 'none'
-}
