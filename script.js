700c3f8a48c627d9e064f9ac1851176513477bff
Timurs Koleda <timurs.koleda@gmail.com>
2025-05-22 19:48:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.getElementById('open-all-img').onclick = function() {
   document.getElementById('open-all-img').style.display = 'none'
-  document.getElementById('all-img-parent').style.transition = '1s'
+  document.getElementById('all-img-parent').style.transition = '0.5s'
   document.getElementById('all-img-parent').style.right = '-15px'
 }
 document.getElementById('close-all-img').onclick = function() {
   document.getElementById('open-all-img').style.display = 'flex'
-  document.getElementById('all-img-parent').style.transition = '1s'
+  document.getElementById('all-img-parent').style.transition = '0.5s'
   document.getElementById('all-img-parent').style.right = '-1000px'
 }
